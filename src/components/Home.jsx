bb28c8161b8aa39e322130c19debcc7a009f61a3
shasank216 <shasankshrestha4@gmail.com>
2022-01-17 14:05:25 +0545

    firstCommit

---
+++ src/components/Home.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import carousel1 from '../assets/caurosel2.jpg'
+import carousel2 from '../assets/caurosel3.jpg'
+import carousel3 from '../assets/caurosel4.jpg'
+import carousel4 from '../assets/caurosel5.jpg'
+import Products from './Products'
+
+
+const Home = () => {
+        return (
+            <div>
+                <div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
+                    <div className="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3" aria-label="Slide 3"></button>
+                    </div>
+                    <div className="carousel-inner">
+                        <div className="carousel-item active" data-bs-interval="10000">
+                            <img src={carousel1} className="d-block w-100" alt="carousel images" />
+                            <div className="carousel-caption d-none d-md-block">
+                            </div>
+                        </div>
+                        <div className="carousel-item" data-bs-interval="2000">
+                            <img src={carousel2} className="d-block w-100" alt="carousel images" />
+                            <div className="carousel-caption d-none d-md-block">
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <img src={carousel3} className="d-block w-100" alt="carousel images" />
+                            <div className="carousel-caption d-none d-md-block">
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <img src={carousel4} className="d-block w-100" alt="carousel images" />
+                            <div className="carousel-caption d-none d-md-block">
+                            </div>
+                        </div>
+                    </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
+                <Products/>
+            </div>
+        )
+    }
+
+export default Home
